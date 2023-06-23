85f607f2bab7c625676e2ac1c99fa08a28f26f81
nafisGB <nafisshuvo3@gmail.com>
2023-06-23 16:53:55 +0600

    bootapp

---
+++ script.js
@@ -0,0 +1,9 @@
+// JavaScript code
+
+// Dark mode toggle
+function toggleDarkMode() {
+  document.body.classList.toggle('dark-mode');
+}
+
+// Event listener for dark mode toggle button
+document.getElementById('darkModeToggle').addEventListener('click', toggleDarkMode);
